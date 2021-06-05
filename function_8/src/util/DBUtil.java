97f2de4fe9fcf70d12ea45aa2e6401c2efd645ed
HolunLi <Holun_Li@163.com>
2021-06-05 23:06:36 +0800

    '修改DBUtil.java'

--- function_8/src/util/DBUtil.java
+++ function_8/src/util/DBUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+import org.apache.commons.dbcp.BasicDataSource;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -14,19 +16,53 @@ public class DBUtil {
     private static PreparedStatement pStatement = null;
     private static ResultSet resultSet = null;
 
-    //从数据连接池中拿连接(通用)
-    public static void connectDatabase() throws ClassNotFoundException, NamingException, SQLException {
-        /*
+    //tomcat-dbcp
+    /*public static void connectDatabase() {
+        *//*
         直接与数据库建立连接(不推荐)
         Class.forName("com.mysql.cj.jdbc.Driver");
-          connection = DriverManager.getConnection(URL, USER, PWD); */
+          connection = DriverManager.getConnection(URL, USER, PWD); *//*
 
-        //初始化查找命名空间
-        Context context = new InitialContext();
-        //获取DataSource。根据数据源名称进行定位,java:comp/env是必须加的,后面跟你的DataSource名。
-        DataSource dataSource = (DataSource) context.lookup("java:comp/env/student");
-        connection = dataSource.getConnection(); //从数据源中拿数据库连接对象(connection对象)
-        System.out.println("hah");
+        try {
+
+            //初始化查找命名空间
+            Context context = context = new InitialContext();
+            //获取DataSource。根据数据源名称进行定位,java:comp/env是必须加的,后面跟你的DataSource名。
+            DataSource dataSource = (DataSource) context.lookup("java:comp/env/student");
+            connection = dataSource.getConnection(); //从连接池中获取数据库连接(connection对象)
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    //dbcp
+    public static void connectDatabase() {
+        //创建数据源basicDataSource
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        //设置数据库驱动类名
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //设置连接数据库的URL
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/mydatabase?useSSL=false&serverTimezone=GMT%2B8");
+        //设置连接数据库的的用户名
+        basicDataSource.setUsername("root");
+        //设置连接数据库的密码
+        basicDataSource.setPassword("haolun");
+        //设置初始化,连接池中的"数据库连接"的数量
+        basicDataSource.setInitialSize(15);
+        //设置连接池中,处于活动状态的数据库连接的最大数量
+        basicDataSource.setMaxActive(10);
+        //设置连接池中,处于空闲状态的数据库连接的最大数量
+        basicDataSource.setMaxIdle(3);
+
+        try {
+            connection = basicDataSource.getConnection(); //从连接池中获取数据库连接
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     //关闭所有的资源(通用)
@@ -53,18 +89,12 @@ public class DBUtil {
 
     //查询操作(通用)
     public static ResultSet query(String sql, String... args) {
+        connectDatabase();
         try {
-            connectDatabase();
             createPreparedStatement(sql, args);
             resultSet = pStatement.executeQuery();
             return resultSet;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
         } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        } catch (NamingException e) {
             e.printStackTrace();
             return null;
         }
